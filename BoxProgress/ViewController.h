2aabf8a8d237e531758eb7f0106f294a641750cf
echgg <echguan@sina.com>
2017-02-25 22:56:29 +0800

    create project

---
+++ BoxProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoxProgress
+//
+//  Created by echgg on 17/2/25.
+//  Copyright © 2017年 micro robot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BoxProgress/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BoxProgress
+//
+//  Created by echgg on 17/2/25.
+//  Copyright © 2017年 micro robot. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
